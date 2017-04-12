925b1b5114d482ab0275b9ddf372d2e8a0d31eb3
JaniKuukkanen <jani.kuukkanen@myy.haaga-helia.fi>
2017-04-12 13:59:29 +0000

    Add SQL statement

--- testi3.py
+++ testi3.py
@@ -2,7 +2,7 @@ import mysql.connector as mariadb
 import RPi.GPIO as GPIO
 import time
 
-mariadb_connection = mariadb.connect(user='pi', password='Qwerty789',database='raspberry')
+mariadb_connection = mariadb.connect(user='pi', password='Qwerty789',host='10.207.5.78',port='3306',database='raspberry')
 cursor = mariadb_connection.cursor()
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
@@ -17,3 +17,22 @@ while True:
 	elif i==1:
 		print("PANIC!!!" + time.strftime("%Y-%m-%d %H:%M:%S"))
 		time.sleep(0.1)
+
+while True:
+	y=GPIO.input(7)
+	sql = ("""INSERT INTO motion (datetime,detected) VALUES (%s,%s)""",(time.strftime("%Y-%m-%d %H:%M:%S"),y))
+	try:
+		print("Writing to database")
+		cur.execute(*sql)
+		db.commit()
+		print("Done'd")
+	
+	except:
+		db.rollback()
+		print("Fatal error!!")
+
+	cur.close()
+	db.close()
+	break
+
+
