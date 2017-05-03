9d324f4ccd26bc461db115e028aca3aabe46738c
JaniKuukkanen <jani.kuukkanen@myy.haaga-helia.fi>
2017-05-03 10:46:18 +0000

    loop works now

--- testi3.py
+++ testi3.py
@@ -2,37 +2,41 @@ import mysql.connector as mariadb
 import RPi.GPIO as GPIO
 import time
 
-mariadb_connection = mariadb.connect(user='pi', password='Qwerty789',host='10.207.5.78',port='3306',database='raspberry')
-cursor = mariadb_connection.cursor()
+mariadb_connection=mariadb.connect(host="10.207.5.78",port=3306,database="raspberry",user="pi3",password="Qwerty789")
+cursor=mariadb_connection.cursor()
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(7, GPIO.IN)
 Timenow = time.strftime("%Y-%m-%d %H:%M:%S")
 
 while True:
+
 	i=GPIO.input(7)
+
+	
 	if i==0:
 	
 		time.sleep(0.1)
 	elif i==1:
 		print("PANIC!!!" + time.strftime("%Y-%m-%d %H:%M:%S"))
-		time.sleep(0.1)
-
-while True:
-	y=GPIO.input(7)
-	sql = ("""INSERT INTO motion (datetime,detected) VALUES (%s,%s)""",(time.strftime("%Y-%m-%d %H:%M:%S"),y))
-	try:
-		print("Writing to database")
-		cur.execute(*sql)
-		db.commit()
-		print("Done'd")
+		time.sleep(5)
 	
-	except:
-		db.rollback()
-		print("Fatal error!!")
 
-	cur.close()
-	db.close()
-	break
+		while i==1:		
+
+			sql = ("""INSERT INTO motion (datetime,detected) VALUES (%s,%s)""",(time.strftime("%Y-%m-%d %H:%M:%S"),i))
+			try:
+				print("Writing to database")
+				cursor.execute(*sql)
+				mariadb_connection.commit()
+				print("Done'd")
+	
+			except:
+				mariadb_connection.rollback()
+				print("Fatal error!!")
+
+		#	cursor.close()
+		#	mariadb_connection.close()
+			break				
 
 
