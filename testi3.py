2704d2e32764d81a34151d528417ff1cfe58bc41
JaniKuukkanen <jani.kuukkanen@myy.haaga-helia.fi>
2017-05-10 11:51:53 +0000

    server connection

--- testi3.py
+++ testi3.py
@@ -1,6 +1,7 @@
 import mysql.connector as mariadb
 import RPi.GPIO as GPIO
 import time
+import os
 
 mariadb_connection=mariadb.connect(host="10.207.5.78",port=3306,database="raspberry",user="pi3",password="Qwerty789")
 cursor=mariadb_connection.cursor()
@@ -20,11 +21,16 @@ while True:
 	elif i==1:
 		print("PANIC!!!" + time.strftime("%Y-%m-%d %H:%M:%S"))
 		time.sleep(5)
-	
+		os.system("fswebcam -r 1280x720 /home/pi/pictures/test"+ time.strftime("%Y-%m-%d_%H:%M:%S") + ".jpg" )
+		os.system("scp /home/pi/pictures/*.jpg projekti@10.207.5.78:/home/projekti/pictures/")
+		time.sleep(5)
+		os.system("sudo rm /home/pi/pictures/*.jpg")	
 
 		while i==1:		
 
 			sql = ("""INSERT INTO motion (datetime,detected) VALUES (%s,%s)""",(time.strftime("%Y-%m-%d %H:%M:%S"),i))
+
+
 			try:
 				print("Writing to database")
 				cursor.execute(*sql)
