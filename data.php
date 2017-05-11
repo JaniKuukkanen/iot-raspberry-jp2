3579d00a03fd0c0271241f62396f6cfeb6c53ecf
JaniKuukkanen <jani.kuukkanen@myy.haaga-helia.fi>
2017-05-11 19:14:53 +0300

    Create data.php

---
+++ data.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+<body>
+<H1>Latest movement:</H1>
+<?php
+$servername = "localhost";
+$username = "pi";
+$password = "Qwerty789";
+$dbname = "raspberry";
+
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+     die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT datetime FROM motion ORDER BY datetime desc LIMIT 10";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+     // output data of each row
+     while($row = $result->fetch_assoc()) {
+         echo "<br> Date and time: ". $row["datetime"]."<br>";
+     }
+} else {
+     echo "0 results";
+}
+
+$conn->close();
+?>
+<?php
+$images = glob('pictures/*.jpg', GLOB_BRACE); //formats to look for
+$images = array_combine(array_map("filemtime", $images), $images);
+krsort($images);
+
+$num_of_files = 10; //number of images to display
+
+foreach($images as $image) {
+
+        $num_of_files--;
+
+         if($num_of_files > -1)
+
+           echo "<br><img src="."'".$image."'"."><br><br>" ; //display images
+         else
+           break;
+    }
+
+?>
+</body>
+</html>
